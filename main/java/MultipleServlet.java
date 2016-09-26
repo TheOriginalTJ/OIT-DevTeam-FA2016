cbe0b189143ecc224ca5c51654e29d83115e0334
kesslerk <kskessler@smcm.edu>
2016-09-25 21:27:13 -0400

    Add files via upload

--- main/java/MultipleServlet.java
+++ main/java/MultipleServlet.java
@@ -2,29 +2,31 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+
 public class MultipleServlet extends HttpServlet {
+    private static final Logger logg = LoggerFactory.getLogger(MultipleServlet.class);
 
-    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) {
-
+    protected void doPost(final HttpServletRequest request, final HttpServletResponse response) {
         response.setContentType("text/html");
+        logg.debug("Successfully connected to form servlet.");
         response.setStatus(HttpServletResponse.SC_OK);
 
 
-        String file_name;
+        String file_name = null;
         FileItem fileItem;
-        InputStream fileInputStream;
+        InputStream fileInputStream = null;
 
+        //get all parameters from the form
         try {
             List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
             String inputName;
@@ -33,18 +35,16 @@ public class MultipleServlet extends HttpServlet {
                 if (!item.isFormField()) {
                     file_name = new File(item.getName()).getName();
                     fileItem = item;
-
-                    System.out.println(file_name);
                     try {
                         fileInputStream = fileItem.getInputStream();
                     } catch (IOException e) {
-                        System.out.println("Bad");
+                        logg.error("Exception while getting file item from form: ", e);
                     }
                 }
             }
         } catch (FileUploadException e) {
-            System.out.println("bad");
+            logg.error("Exception while uploading file: ", e);
         }
-
+        System.out.println(file_name);
     }
 }

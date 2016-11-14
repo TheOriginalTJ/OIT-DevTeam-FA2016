cce97d955b94c550fed5f70ac1d962cf4bcf1a19
kesslerk <kskessler@smcm.edu>
2016-11-14 12:22:39 -0500

    Create ClassList.java

---
+++ src/main/java/GUI_Dev/ClassList.java
@@ -0,0 +1,19 @@
+package GUI_Dev;
+import java.util.List;
+
+import OIT_Dev.Class;
+
+
+public class ClassList {
+	private List<Class> classList;
+
+	public List<Class> getClassList() {
+		return classList;
+	}
+
+	public void setClassList(List<Class> list) {
+		this.classList = list;
+	}
+	
+
+}

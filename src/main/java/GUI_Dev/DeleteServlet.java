dee2f29f561da0b19959c78453edd08da91c2d52
kesslerk <kskessler@smcm.edu>
2016-11-16 12:38:14 -0500

    Add files via upload

--- src/main/java/GUI_Dev/DeleteServlet.java
+++ src/main/java/GUI_Dev/DeleteServlet.java
@@ -50,33 +50,6 @@ public class DeleteServlet extends HttpServlet {
         /*for(int i=0; i<filtered.size(); i++){
         	System.out.println(filtered.get(i).getCourseName());
         }*/
-       /* try {
-			response.getWriter().print("<button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Open Modal</button>");
-
-<!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
     
     }
 
